7fb00aaba68fffbb754d61310f7c93600678c155
Lucas Coronel <lucas.coronel@platformbuilders.io>
2023-02-24 08:05:02 -0300

    feat: send console.log 2

--- src/domain/event/customer/handler/send-email-when-customer-is-created.spec.ts
+++ src/domain/event/customer/handler/send-email-when-customer-is-created.spec.ts
@@ -3,14 +3,19 @@ import { Customer } from "../../../entity/Customer";
 import { EventDispatcher } from "../../@shared/event-dispatcher";
 import { CustomerCreatedEvent } from "../customer-created.event";
 import { SendEmailWhenCustomerIsCreatedHandler } from "./send-email-when-customer-is-created.handler";
+import { SendEmailWhenCustomerIsCreatedHandler2 } from "./send-email-when-customer-is-created2.handler";
 
 describe("Customer handler tests", () => {
     it('should notify when customer is created', () => {
         const eventDispatcher = new EventDispatcher();
         const eventHandler = new SendEmailWhenCustomerIsCreatedHandler();
+        const eventHandler2 = new SendEmailWhenCustomerIsCreatedHandler2();
+
         const spyEventHandler = jest.spyOn(eventHandler, "handle")
+        const spyEventHandler2 = jest.spyOn(eventHandler2, "handle")
 
         eventDispatcher.register('CustomerCreatedEvent', eventHandler)
+        eventDispatcher.register('CustomerCreatedEvent', eventHandler2)
 
         let customer = new Customer("123", "Lucas")
         const address = new Address("Rua dois", 2, "12312312", "Sao Paulo")
@@ -25,5 +30,6 @@ describe("Customer handler tests", () => {
         eventDispatcher.notify(customerCreatedEvent)
 
         expect(spyEventHandler).toHaveBeenCalled()
+        expect(spyEventHandler2).toHaveBeenCalled()
     })
 })
